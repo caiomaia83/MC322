5aa5c580ff11efff9263df3919c439f1761fd99b
Fernando Nagano Foschiera <fnfoschiera@localhost.localdomain>
2025-09-01 21:32:44 -0300

    prototipo personagem

---
+++ tarefa1/src/Heroi.java
@@ -0,0 +1,13 @@
+public class Heroi extends Personagem{
+    int nivel;
+    int experiencia;
+
+    public Heroi() {
+        
+          
+    }
+
+    public int atacar() {
+        return this.forca;
+    }
+}
---
+++ tarefa1/src/Personagem.java
@@ -0,0 +1,24 @@
+public abstract class Personagem {
+    String nome;
+    int pontosDeVida;
+    int pontosDeVidaTotal;
+    int forca;
+
+    public Personagem(String nome, int pontosDeVidaTotal, int forca) {
+        this.forca = forca;
+        this.nome = nome;
+        this.pontosDeVida = this.pontosDeVidaTotal = pontosDeVidaTotal;
+    }
+
+    public void receberDano(int dano) {
+        this.pontosDeVida -= dano;
+    }
+
+    public void exibirStatus() {
+        System.out.printf(" -- Status de %d --\n", this.nome);
+        System.out.printf("HP: %d / %d\n", this.pontosDeVida, this.pontosDeVidaTotal);
+        System.out.printf("Forca: %d\n", this.forca);
+    }
+
+    public abstract int atacar();
+}
