e5cd6b394a6daf104b8806c4621f79d7221f2e30
Fernando Nagano Foschiera <fnfoschiera@localhost.localdomain>
2025-10-05 18:18:40 -0300

    Comentei todos ações de combate e os ataques

--- tarefa1/src/com/rpg/combate/InvestidaReal.java
+++ tarefa1/src/com/rpg/combate/InvestidaReal.java
@@ -2,10 +2,29 @@ package com.rpg.combate;
 
 
 import com.rpg.personagens.*;
-import com.rpg.personagens.Heroi;
 
+/**
+ * Representa a habilidade especial "Investida Real", habilidade especial do Recruta Real.
+ * <p>
+ * É uma manobra de alto risco e alta recompensa. Possui uma alta chance (80%)
+ * de causar um dano crítico massivo, mas também uma pequena chance (20%) de falhar
+ * completamente, não causando dano algum.
+ * </p>
+ *
+ * @author Fernando e Caio
+ * @version 1.0
+ * @since 2025-10-05
+ */
 public class InvestidaReal implements AcaoCombate {
-
+        /**
+     * {@inheritDoc}
+     * <p>
+     * Executa a Investida Real. O resultado é baseado na sorte do {@link Heroi}:
+     * 
+     * -Sucesso (Sorte >= 0.20): O ataque acerta e causa dano crítico. O multiplicador de dano é dinâmico, sendo {@code (1.5 + sorte)}, resultando em um dano entre 165% e 250%.
+     * -Falha (Sorte < 0.20): O herói escorrega, e o ataque falha, causando 0 de dano.
+     * </p>
+     */
     @Override
     public void executar(Combatente usuario, Combatente alvo) {
         System.out.println("--> HABILIDADE ESPECIAL! <--");
@@ -15,7 +34,7 @@ public class InvestidaReal implements AcaoCombate {
         if (usuario instanceof Heroi) {
             Heroi heroiUsuario = (Heroi) usuario;
 
-            if (heroiUsuario.getSorte() >= 0.15) {
+            if (heroiUsuario.getSorte() >= 0.2) {
                 // A lógica de dano bônus que estava no 'atacar' vem para cá
                 int dano = (int) (heroiUsuario.getDano() * (1.5 + heroiUsuario.getSorte()));
                 System.out.println(heroiUsuario.getNome() + " atacou com Investida Real e causou dano crítico!");
